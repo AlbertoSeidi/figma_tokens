e458aadeb41079581baf4082a32fe39b4c1cb480
Alberto Seidi <seiji_alberto@hotmail.com>
2024-06-21 16:49:03 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,18 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 21 Jun 2024 19:04:16 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorPurple": "#885cf6",
+  "ColorGrey200": "#c1c1c4",
+  "ColorGrey500": "#50505c",
+  "ColorGrey800": "#1c1c1f",
+  "ColorGrey900": "#121214",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionPurple500": "#8257e6ff",
+  "VariableCollectionGrey200": "#c1c1c4ff",
+  "VariableCollectionGrey500": "#5c5959ff",
+  "VariableCollectionGrey800": "#1c1c1fff"
+};
